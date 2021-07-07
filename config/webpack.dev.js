1c3e9152d00b0f1a6c8813242c18efc1e58f4798
Шурыгина Юлия Сергеевна <yushurygina@phoenixit.ru>
2021-07-07 17:18:24 +0300

    start Header

---
+++ config/webpack.dev.js
@@ -0,0 +1,43 @@
+const webpack = require('webpack')
+const { merge } = require('webpack-merge')
+
+const common = require('./webpack.common.js')
+const paths = require('./paths')
+
+module.exports = merge(common, {
+    mode: 'development',
+
+    devtool: 'inline-source-map',
+
+    devServer: {
+        historyApiFallback: true,
+        contentBase: paths.build,
+        open: true,
+        compress: true,
+        hot: true,
+        port: 3002,
+    },
+
+    module: {
+        rules: [{
+                test: /\.css$/,
+                use: ["style-loader", "css-loader", 'postcss-loader']
+            }
+            // {
+            //   test: /\.(scss|css)$/,
+            //   use: [
+            //     'style-loader',
+            //     {
+            //       loader: 'css-loader',
+            //       options: { sourceMap: true, importLoaders: 1, modules: true },
+            //     },
+            //     { loader: 'postcss-loader', options: { sourceMap: true } },
+            //   ],
+            // },
+        ],
+    },
+
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+    ],
+})
