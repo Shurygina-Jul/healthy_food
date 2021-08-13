53d384611a7f130c53234ff5ed50662fb8070cfd
Шурыгина Юлия Сергеевна <yushurygina@phoenixit.ru>
2021-08-13 14:21:54 +0300

    fix adaptive 320px

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
 import "@/styles/main.css";
+import { KeyDownCode } from "@/js/constants";
+// import { openBurger } from "@/js/burger";
+
+// openBurger();
---
+++ src/js/constants.js
@@ -0,0 +1,4 @@
+export const KeyDownCode = {
+  ESC: 27,
+  ESCAPE: `Escape`,
+};
